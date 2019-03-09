39358b0df9a20e8ca67a7af97b1bba3c1872b119
choongyon.lee <choongyon.lee@bankwareglobal.com>
2019-03-09 10:48:53 +0900

    viewcnt isolation 추가

--- SimpleBoard/src/main/java/org/simple/service/BoardServiceImpl.java
+++ SimpleBoard/src/main/java/org/simple/service/BoardServiceImpl.java
@@ -11,6 +11,8 @@ import org.simple.mapper.BoardMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BoardServiceImpl implements BoardService
@@ -26,6 +28,7 @@ public class BoardServiceImpl implements BoardService
 		dao.create(board);
 	}
 
+	@Transactional(isolation=Isolation.READ_COMMITTED)
 	@Override
 	public BoardVO read(Integer bno) throws Exception
 	{
